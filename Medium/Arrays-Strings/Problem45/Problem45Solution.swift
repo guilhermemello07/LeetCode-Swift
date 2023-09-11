8d7931b52b82cb47dc06ba6655c725b4f871c84c
Guilherme Teixeira de Mello <72673965+guilhermemello07@users.noreply.github.com>
2023-09-11 10:06:20 -0300

    Create Problem45Solution.swift

---
+++ Medium/Arrays-Strings/Problem45/Problem45Solution.swift
@@ -0,0 +1,58 @@
+/*
+ Problem 45 - Jump Game II
+ Medium - Arrays&Strings
+ 
+ You are given a 0-indexed array of integers nums of length n. You are initially positioned at nums[0].
+
+ Each element nums[i] represents the maximum length of a forward jump from index i. In other words, if you are at nums[i], you can jump to any nums[i + j] where:
+
+ 0 <= j <= nums[i] and
+ i + j < n
+ Return the minimum number of jumps to reach nums[n - 1]. The test cases are generated such that you can reach nums[n - 1].
+
+  
+
+ Example 1:
+
+ Input: nums = [2,3,1,1,4]
+ Output: 2
+ Explanation: The minimum number of jumps to reach the last index is 2. Jump 1 step from index 0 to 1, then 3 steps to the last index.
+ Example 2:
+
+ Input: nums = [2,3,0,1,4]
+ Output: 2
+  
+
+ Constraints:
+
+ 1 <= nums.length <= 104
+ 0 <= nums[i] <= 1000
+ It's guaranteed that you can reach nums[n - 1].
+ */
+
+func jump(_ nums: [Int]) -> Int {
+    
+    var jumps = 0
+    var leftPointer = 0
+    var rightPointer = 0
+    var farthest = 0
+    
+    while rightPointer < nums.count - 1 {
+        for i in leftPointer..<rightPointer + 1 {
+            farthest = max(farthest, i + nums[i])
+            
+        }
+        leftPointer = rightPointer + 1
+        rightPointer = farthest
+        jumps += 1
+        
+    }
+    return jumps
+}
+
+//testing
+var nums1 = [2,3,1,1,4]
+print(jump(nums1))
+
+var nums2 = [2,3,0,1,4]
+print(jump(nums2))
