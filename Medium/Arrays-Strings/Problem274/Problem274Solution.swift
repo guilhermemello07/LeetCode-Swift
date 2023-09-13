536e590a1dec8c403f4ff9e0c92e4afb4a11bd18
Guilherme Teixeira de Mello <72673965+guilhermemello07@users.noreply.github.com>
2023-09-13 10:39:01 -0300

    Create Problem274Solution.swift

---
+++ Medium/Arrays-Strings/Problem274/Problem274Solution.swift
@@ -0,0 +1,49 @@
+/*
+ Problem 274 - H-Index
+ Medium - Arrays&Strings
+ 
+ Given an array of integers citations where citations[i] is the number of citations a researcher received for their ith paper, return the researcher's h-index.
+
+ According to the definition of h-index on Wikipedia: The h-index is defined as the maximum value of h such that the given researcher has published at least h papers that have each been cited at least h times.
+
+  
+
+ Example 1:
+ Input: citations = [3,0,6,1,5]
+ Output: 3
+ Explanation: [3,0,6,1,5] means the researcher has 5 papers in total and each of them had received 3, 0, 6, 1, 5 citations respectively.
+ Since the researcher has 3 papers with at least 3 citations each and the remaining two with no more than 3 citations each, their h-index is 3.
+ 
+ 
+ Example 2:
+ Input: citations = [1,3,1]
+ Output: 1
+  
+
+ Constraints:
+
+ n == citations.length
+ 1 <= n <= 5000
+ 0 <= citations[i] <= 1000
+ 
+ */
+
+func hIndex(_ citations: [Int]) -> Int {
+
+    var sortedCitations = Array(citations.sorted().reversed())
+    
+    for (i, citation) in sortedCitations.enumerated() {
+        if i >= citation {
+            return i
+        }
+    }
+    return sortedCitations.count
+}
+
+
+//Testing
+var citations1 = [3,0,6,1,5]
+print(hIndex(citations1)) //3
+
+var citations2 = [1,3,1]
+print(hIndex(citations2)) //1
